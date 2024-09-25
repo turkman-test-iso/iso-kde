d2882f441b094b282a077ce2b4f1255ee3f95f63
sulincix <aliriza.keskin@pardus.org.tr>
2024-09-25 10:32:00 +0000

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,21 +1,58 @@
 #!/bin/sh
-#X11 install
+# enable live
+ymp repo --update --ignore-gpg --allow-oem
+ymp install shadow audit --no-emerge --allow-oem
+ymp install e2fsprogs dialog grub parted dosfstools rsync --no-emerge --allow-oem
+rm -f /sbin/init
+wget https://gitlab.com/turkman/devel/sources/installer/-/raw/master/main.sh -O /sbin/init
+chmod 755 /sbin/init
+# install x11
 ymp repo --update --allow-oem --ignore-gpg
-ymp it xinit xorg-server xterm freetype xauth xkbcomp xkeyboard-config @x11.drivers --no-emerge --allow-oem --jobs=1
-ymp it elogind libtool shadow pipewire wireplumber fuse fuse2 --no-emerge --allow-oem --jobs=1
+ymp it xinit xorg-server xterm freetype xauth xkbcomp xkeyboard-config @x11.drivers --no-emerge --allow-oem
+ymp it elogind shadow pipewire wireplumber libtool firefox-installer fuse fuse2 --no-emerge --allow-oem
 # install kde
-ymp it @kde.plasma @kde.frameworks dolphin konsole dejavu tzdata --no-emerge --allow-oem --jobs=1
-echo "startplasma-x11" > /data/user/root/.xinitrc
-sed -i "s/#USER/USER/g" /etc/conf.d/xinit
+ymp it @kde.plasma @kde.frameworks dolphin konsole ark dejavu tzdata --no-emerge --allow-oem --jobs=1
+# fstab add tmpfs
 echo "tmpfs /tmp tmpfs rw 0 0" > /etc/fstab
 ln -s /proc/mounts /etc/mtab
-sed -i "s|#agetty_options.*|agetty_options=\"-l /usr/bin/login\"|" /etc/conf.d/agetty
-chmod u+s /usr/bin/su /usr/lib64/polkit-1/*
+# enable login from shadow
+sed -i "s|#agetty_options.*|agetty_options=\" -l /usr/bin/login\"|" /etc/conf.d/agetty
+chmod u+s /bin/su /usr/bin/su
+# set language
+mkdir -p /lib64/locale
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "" >> /etc/locale.gen
+echo "export LANG=en_US.UTF-8" > /etc/profile.d/locale.sh
+echo "export LC_ALL=en_US.UTF-8" >> /etc/profile.d/locale.sh
+locale-gen
+# polkit enable
+chmod u+s /usr/bin/pkexec /usr/lib64/polkit-1/polkit-agent-helper-1
+echo "/bin/bash" > /etc/shells
+echo "/bin/sh" >> /etc/shells
+echo "/bin/ash" >> /etc/shells
+# remove static libraries
+find / -type f -iname '*.a' -exec rm -f {} \;
+# install wifi and bluetooth
+ymp it wpa_supplicant networkmanager bluez --no-emerge --allow-oem
+# install lightdm
+ymp it sddm --no-emerge --allow-oem
+# update hicolor icons
+gtk-update-icon-cache /usr/share/icons/hicolor/
+# enable services
 rc-update add elogind
 rc-update add eudev
 rc-update add fuse
-rc-update add udhcpc
+rc-update add seatd
+rc-update add upowerd
 rc-update add hostname
+rc-update add wpa_supplicant
 rc-update add networkmanager
+rc-update add sddm
+rc-update add bluetooth
+rc-update add polkit
+rc-update add touchegg
 ymp clean --allow-oem
+# revert hardened bindir
+mkdir -p /usr/local/bin
+chmod 755 /bin /usr/bin /sbin /usr/sbin /usr/local/bin
 exit 0
