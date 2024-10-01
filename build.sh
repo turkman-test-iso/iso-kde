56bacd794c0d03f9aea11ad7589628e5c502fddc
mobilturka <162546868+03tekno@users.noreply.github.com>
2024-10-01 19:13:18 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -ex
 mkdir /output -p
-apt update || true
-apt install git grub-pc-bin grub-efi grub-efi-ia32-bin squashfs-tools mtools xorriso rdfind -y || true
-export FIRMWARE=1
+apt update
+apt install git grub-pc-bin grub-efi grub-efi-ia32-bin squashfs-tools mtools xorriso rdfind -y
 function build(){
     variant=$1
     suffix=$2
@@ -12,14 +11,16 @@ function build(){
     if [ -f ../$variant.sh ] ; then
         install ../$variant.sh custom
     fi
-    sed -i "s/console=tty31//g" mkiso.sh
     bash -ex mkiso.sh
     mv turkman.iso /output/turkman-$variant$suffix.iso
-    echo "##### $(date) #####" > /output/turkman-$variant$suffix.revdep-rebuild
     mount -t proc proc rootfs/proc
     chroot rootfs ymp rbd --no-color 2>/dev/null | tee -a /output/turkman-$variant$suffix.revdep-rebuild
     umount -lf rootfs/proc
     cd ..
     rm -rf $variant$suffix
 }
-build kde-enduser
+for variant in kde; do
+    export FIRMWARE=1
+    suffix="-firmware"
+    build $variant $suffix
+done
